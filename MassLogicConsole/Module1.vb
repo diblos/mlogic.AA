e9d1f6b2f811d87cfa0a538b615421d76aca3027
diblos <diblos@gmail.com>
2017-07-23 09:41:18 +0800

    testing run

--- MassLogicConsole/Module1.vb
+++ MassLogicConsole/Module1.vb
@@ -10,50 +10,56 @@ Imports System.Security.Cryptography.X509Certificates
 Imports System.Text
 Imports System.Xml
 
-Module Module1
 
-    Private Const driveLetter As String = "C:\\"
+Namespace MassLogicConsole
+    Module Module1
 
-    Private Const dirPath As String = "C:\Airbus\LPC-NG\report"
+        Private Const DateStr As String = "yyyy-MM-dd HH:mm"
 
-    Private Const extToSearch As String = "*.xml"
+        Private Const driveLetter As String = "C:\\"
 
-    Private Const filenameSplitChar As Char = "-"c
+        Private Const dirPath As String = "C:\Airbus\LPC-NG\report"
 
-    Private Const numberOfSplits As Integer = 4
+        Private Const extToSearch As String = "*.xml"
 
-    Private Const workspaceRoomId As Integer = 339569
+        Private Const filenameSplitChar As Char = "-"c
 
-    Private Const certFilename As String = "MassLogicCert.pfx"
+        Private Const numberOfSplits As Integer = 4
 
-    Private Const certPassword As String = "masslogicshukor"
+        Private Const workspaceRoomId As Integer = 339569
 
-    Private Const workspaceServerUrl As String = "shukor.watchdox.com"
+        Private Const certFilename As String = "MassLogicCert.pfx"
 
-    Private Const userEmail As String = "msahmad82@gmail.com"
+        Private Const certPassword As String = "masslogicshukor"
 
-    Private Const serviceAccountIssuerName As String = "com.watchdox.system.0367.3855"
+        Private Const workspaceServerUrl As String = "shukor.watchdox.com"
 
-    Private Const tokenExpiresInMinutes As Integer = 5
+        Private Const userEmail As String = "msahmad82@gmail.com"
 
-    'Shared?
-    Private apiSession As ApiSession
-    Private VolumeSerialNumber As String
-    Private VolumeSerialNumberHex As String
-    Private liReportFile As List(Of ReportFile)
-    Private liGroups As List(Of String)
-    Private liDomains As List(Of String)
-    Private Declare Function GetVolumeInformation Lib "kernel32.dll" (PathName As String, VolumeNameBuffer As StringBuilder, VolumeNameSize As UInteger, ByRef VolumeSerialNumber As UInteger, ByRef MaximumComponentLength As UInteger, ByRef FileSystemFlags As UInteger, FileSystemNameBuffer As StringBuilder, FileSystemNameSize As UInteger) As Long
-    'Shared?
+        Private Const serviceAccountIssuerName As String = "com.watchdox.system.0367.3855"
+
+        Private Const tokenExpiresInMinutes As Integer = 5
+
+        'Shared?
+        'Private apiSession As ApiSession
+        'Private VolumeSerialNumber As String
+        'Private VolumeSerialNumberHex As String
+        'Private liReportFile As List(Of ReportFile)
+        'Private liGroups As List(Of String)
+        'Private liDomains As List(Of String)
+        'Private Declare Function GetVolumeInformation Lib "kernel32.dll" (PathName As String, VolumeNameBuffer As StringBuilder, VolumeNameSize As UInteger, ByRef VolumeSerialNumber As UInteger, ByRef MaximumComponentLength As UInteger, ByRef FileSystemFlags As UInteger, FileSystemNameBuffer As StringBuilder, FileSystemNameSize As UInteger) As Long
+        'Shared?
 
 
-    Sub Main()
-        HappyEnd() 'Wait input to end
-    End Sub
+        Sub Main()
+            Console.WriteLine(Now.ToString(DateStr))
+            HappyEnd() 'Wait input to end
+        End Sub
 
-    Sub HappyEnd()
-        Dim r = Console.ReadLine()
-        Console.WriteLine(r)
-    End Sub
+        Sub HappyEnd()
+            Dim r = Console.ReadLine()
+            Console.WriteLine(r)
+        End Sub
 
-End Module
+    End Module
+End Namespace
--- MassLogicConsole/ReportFile.vb
+++ MassLogicConsole/ReportFile.vb
@@ -1,51 +1,54 @@
-﻿Friend Class ReportFile
-    Private VSNfromFilename As String
+﻿
+Namespace MassLogicConsole
+    Friend Class ReportFile
+        Private VSNfromFilename As String
 
-    Private dateTimeFromFilename As String
+        Private dateTimeFromFilename As String
 
-    Private dstFilename As String
+        Private dstFilename As String
 
-    Public Property dateTimeFromContent() As String
+        Public Property dateTimeFromContent() As String
 
-    Public Property absolutePath() As String
+        Public Property absolutePath() As String
 
-    Public Property fileName() As String
+        Public Property fileName() As String
 
-    Public Property OISVersion() As String
+        Public Property OISVersion() As String
 
-    Public Property platformName() As String
+        Public Property platformName() As String
 
-    Public Property platformType() As String
+        Public Property platformType() As String
 
-    Public Property WatchdoxFileContent() As String
+        Public Property WatchdoxFileContent() As String
 
-    Public Sub New(absolutePath As String, fileName As String, VSNfromFilename As String, dateTimeFromFilename As String)
-        Me.absolutePath = absolutePath
-        Me.fileName = fileName
-        Me.VSNfromFilename = VSNfromFilename
-        Me.dateTimeFromFilename = dateTimeFromFilename
-        Me.dstFilename = Me.getDstFilename(dateTimeFromFilename)
-    End Sub
+        Public Sub New(absolutePath As String, fileName As String, VSNfromFilename As String, dateTimeFromFilename As String)
+            Me.absolutePath = absolutePath
+            Me.fileName = fileName
+            Me.VSNfromFilename = VSNfromFilename
+            Me.dateTimeFromFilename = dateTimeFromFilename
+            Me.dstFilename = Me.getDstFilename(dateTimeFromFilename)
+        End Sub
 
-    Public Function getDstFilename() As String
-        Return Me.dstFilename
-    End Function
+        Public Function getDstFilename() As String
+            Return Me.dstFilename
+        End Function
 
-    Public Function getVSNfromFilename() As String
-        Return Me.VSNfromFilename
-    End Function
+        Public Function getVSNfromFilename() As String
+            Return Me.VSNfromFilename
+        End Function
 
-    Private Function getDstFilename(dateTimeFromFilename As String) As String
-        Dim str As String = Nothing
-        Dim str2 As String = Nothing
-        Dim str3 As String = Nothing
-        Try
-            Dim expr_0E As String = dateTimeFromFilename.Substring(0, 8)
-            str = expr_0E.Substring(0, 4)
-            str2 = expr_0E.Substring(4, 2)
-            str3 = expr_0E.Substring(6, 2)
-        Catch ex_2A As ArgumentOutOfRangeException
-        End Try
-        Return str3 + str2 + str
-    End Function
-End Class
+        Private Function getDstFilename(dateTimeFromFilename As String) As String
+            Dim str As String = Nothing
+            Dim str2 As String = Nothing
+            Dim str3 As String = Nothing
+            Try
+                Dim expr_0E As String = dateTimeFromFilename.Substring(0, 8)
+                str = expr_0E.Substring(0, 4)
+                str2 = expr_0E.Substring(4, 2)
+                str3 = expr_0E.Substring(6, 2)
+            Catch ex_2A As ArgumentOutOfRangeException
+            End Try
+            Return str3 + str2 + str
+        End Function
+    End Class
+End Namespace
